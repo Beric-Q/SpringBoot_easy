053125e868476de9378f4bbabbe2f804e759cd70
virusgipcle <779343632@qq.com>
2021-10-30 17:49:47 +0800

    git仓库测试

---
+++ src/main/java/com/example/demo/git_test/first_test.java
@@ -0,0 +1,8 @@
+package com.example.demo.git_test;
+
+public class first_test {
+    public static void main(String[] args) {
+        System.out.println("if u can see this sentence");
+        System.out.println("then u success");
+    }
+}
