9ae1a4f80de550c1617959bd93f74305bd270243
Beric-Q <779343632@qq.com>
2021-10-31 12:43:19 +0800

    实验室git测试

---
+++ src/main/java/com/example/demo/git_test/lab_computer_test.java
@@ -0,0 +1,7 @@
+package com.example.demo.git_test;
+
+public class lab_computer_test {
+    public static void main(String[] args) {
+        System.out.println("a test for git in lab computer");
+    }
+}
