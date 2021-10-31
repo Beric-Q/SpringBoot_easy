63486b5521f2ca8e9a132371637facadb41c1bee
Beric-Q <779343632@qq.com>
2021-10-31 12:46:14 +0800

    Revert "实验室git测试"
    This reverts commit 9ae1a4f80de550c1617959bd93f74305bd270243.

--- src/main/java/com/example/demo/git_test/lab_computer_test.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.git_test;
-
-public class lab_computer_test {
-    public static void main(String[] args) {
-        System.out.println("a test for git in lab computer");
-    }
-}
